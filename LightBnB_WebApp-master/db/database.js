2ec9a813d6911791fc8ed089c07ed82f1da69081
Tobi Onipede <onipede89@gmail.com>
2023-08-11 04:02:51 +0100

    implement getAllReservations to pull from our LightBnb Database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -103,7 +103,27 @@ RETURNING *
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const queryString = `
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+  `;
+
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 /// Properties
